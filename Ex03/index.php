a89854e467d29400b94454fdd3e1fa19fb6054df
LarissaSirioFerraz <larissasirioferraz@gmail.com>
2025-04-15 10:32:58 -0300

    Programa que pede para escolher entre duas opções (homem e mulher) e um botão para enviar a resposta.

---
+++ Ex03/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE HTML>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Frase de sorte</title>
+</head>
+<body>
+<fieldset>
+    <legend>Frase de sorte</legend>
+    <form method="post" action="calcular.php">
+        
+         <p>Escolha seu gênero:</p>
+
+        <input type="radio" name="genero" value="homem"> Homem
+
+        <input type="radio" name="genero" value="mulher"> Mulher
+		<br />
+		<br />
+        <button type="submit">Enviar</button>
+        <br />
+    </form>
+</fieldset>
+</body>
+</html>
