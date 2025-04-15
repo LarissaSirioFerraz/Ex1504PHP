d5b410524557f71864dd902fd68c61e24044d3f2
LarissaSirioFerraz <larissasirioferraz@gmail.com>
2025-04-15 09:29:51 -0300

    Programa que gera um número aleatório de 0 a 99 e imprime na tela.

---
+++ Ex01/calcular.php
@@ -0,0 +1,15 @@
+<!DOCTYPE HTML>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Resultado</title>
+</head>
+<body>
+<?php
+
+    $ns = rand(0,99);
+
+	echo "O número gerado foi: ". $ns;
+?>
+</body>
+</html>
