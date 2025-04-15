4975292397b3d59562549d52432bdce8beaa7db0
LarissaSirioFerraz <larissasirioferraz@gmail.com>
2025-04-15 09:39:00 -0300

    Programa que exibe na tela pedindo a inserção de um número e um botão que gera um número aleatório.

---
+++ Ex02/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE HTML>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Sorteio</title>
+</head>
+<body>
+<fieldset>
+    <legend>Número Aleatório</legend>
+    <form method="post" action="calcular.php">
+	    Digite um número:
+        <input type="text" name="n1" size="25" />
+        <br />
+        <button type="submit">Gerar</button>
+        <br />
+    </form>
+</fieldset>
+</body>
+</html>
